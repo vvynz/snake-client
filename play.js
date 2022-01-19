4c20dfd34afc74f234c6afe0c7dd92e7fecbaee3
Vivian Zhang <vvynz12@gmail.com>
2022-01-19 21:50:54 +0000

    setup the setupIntput and handleUserInput functions in play.js. The code can receive and recognize when a user ctrl + c in the program

--- play.js
+++ play.js
@@ -1,2 +1,24 @@
-const net = require("net");
 const { connect } = require("./client");
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function () {
+  process.stdin.on("data", (data) => {
+    if (data === "\u0003") {
+      process.exit();
+    }
+  });
+};
+
+console.log("Connecting ...");
+setupInput();
+connect();
